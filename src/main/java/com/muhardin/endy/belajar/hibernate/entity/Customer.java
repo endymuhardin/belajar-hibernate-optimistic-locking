8c5ff4c2fd80597cb8f74a0c5173c9d72e002dd5
Endy Muhardin <endy.muhardin@gmail.com>
2014-07-01 10:47:27 +0700

    fix nama tabel

--- src/main/java/com/muhardin/endy/belajar/hibernate/entity/Customer.java
+++ src/main/java/com/muhardin/endy/belajar/hibernate/entity/Customer.java
@@ -4,9 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Version;
 
-@Entity
+@Entity @Table(name = "t_customer")
 public class Customer {
     
     @Id @GeneratedValue
