eacceae8ec25f6b7750dfc811313f57bdc37407a
Endy Muhardin <endy.muhardin@gmail.com>
2014-07-01 09:58:43 +0700

    entity class

---
+++ src/main/java/com/muhardin/endy/belajar/hibernate/entity/Customer.java
@@ -0,0 +1,47 @@
+package com.muhardin.endy.belajar.hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Customer {
+    
+    @Id @GeneratedValue
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Version
+    @Column(nullable = false)
+    private Integer version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+    
+    
+}
